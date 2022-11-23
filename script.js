84113ff9966c3fcf9193a5eb1dad96d74a79cddf
Mobeen <mmobeen1@gmail.com>
2022-11-22 22:17:43 -0600

    ALT

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function initMap() {
   var map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 21.435376, lng: -39.824574 },
-    zoom: 8,
+    zoom: 5,
 	zoomControl: false,
     scaleControl: true,
 	mapTypeId: 'hybrid'
