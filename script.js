eee3b6acfa07cab0994b3ba3c7e440878746401b
Mobeen <mmobeen1@gmail.com>
2022-11-22 22:14:03 -0600

    alt

---
+++ script.js
@@ -0,0 +1,20 @@
+function initMap() {
+  var map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 21.435376, lng: -39.824574 },
+    zoom: 8,
+	zoomControl: false,
+    scaleControl: true,
+	mapTypeId: 'hybrid'
+  });
+  
+  var marker = new google.maps.Marker({
+    position: { lat: 21.435376 , lng: -39.824574},
+    map: map,
+  });
+  
+  var infowindow = new google.maps.InfoWindow({
+    content:"The clock Tower"
+  });
+  infowindow.open(map, marker);
+}
+window.initMap = initMap;
